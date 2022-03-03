9f64231ffb1fb79087d34d847dd39aa100487c9f
bwayne500 <bwayne500elite@gmail.com>
2022-03-03 16:22:14 +0800

    Initial Commit

---
+++ Bus/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Bus
+//
+//  Created by bwayne500 on 2022/3/3.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
